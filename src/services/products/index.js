2c1d8e9c091172fce7f756bcf09a9e8455a3c350
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-09-29 15:53:42 +0200

    put getid delete

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -6,10 +6,10 @@ router
   .route("/")
   .get(async (req, res, next) => {
     try {
-        const products = await product.findAll({
-            include:review
-        })
-        res.send(products)
+      const products = await product.findAll({
+        include: review,
+      });
+      res.send(products);
     } catch (error) {
       console.log(error);
       next(error);
@@ -17,12 +17,53 @@ router
   })
   .post(async (req, res, next) => {
     try {
-        const products = await product.create(req.body)
-        res.send(products)
+      const products = await product.create(req.body);
+      res.send(products);
     } catch (error) {
       console.log(error);
       next(error);
     }
   });
-
+router
+  .route("/:id")
+  .get(async (req, res, next) => {
+    try {
+      const products = await product.findByPk(req.params.id);
+      res.send(products);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+  .put(async (req, res, next) => {
+    try {
+        const products = await product.update(req.body, {
+            where: {
+                id: req.params.id
+            },
+            returning: true
+        })
+        res.send(products[1][0])
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+  .delete(async (req, res, next) => {
+      try {
+          const rows = await product.destroy({
+              where: {
+                  id: req.params.id
+              }
+          })
+          if (rows > 0) {
+              res.send("done")
+          } else {
+              res.status(404).send("product not found")
+          }
+      } catch (error) {
+        console.log(error);
+        next(error);      
+      }
+  })
 export default router;
--- src/services/reviews/index.js
+++ src/services/reviews/index.js
@@ -24,5 +24,46 @@ router
       next(error);
     }
   });
-
+  router
+  .route("/:id")
+  .get(async (req, res, next) => {
+    try {
+      const reviews = await review.findByPk(req.params.id);
+      res.send(reviews);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+  .put(async (req, res, next) => {
+    try {
+        const reviews = await review.update(req.body, {
+            where: {
+                id: req.params.id
+            },
+            returning: true
+        })
+        res.send(reviews[1][0])
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+  .delete(async (req, res, next) => {
+      try {
+          const rows = await review.destroy({
+              where: {
+                  id: req.params.id
+              }
+          })
+          if (rows > 0) {
+              res.send("done")
+          } else {
+              res.status(404).send("review not found")
+          }
+      } catch (error) {
+        console.log(error);
+        next(error);      
+      }
+  })
 export default router;
